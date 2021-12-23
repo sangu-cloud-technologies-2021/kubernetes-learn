c580448eaac2a87f7762826a63c0c7e338cfdeb6
David Abulashvili <dato@raizomat.com>
2021-12-23 17:02:46 +0100

    added front template and service code

---
+++ service/app.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+const { port, mongoHost } = require('./config');
+const { MongoClient } = require('mongodb');
+const mongoUrl = `mongodb://${mongoHost}:27017`;
+
+const mongoClient = new MongoClient(mongoUrl);
+
+const dbName = 'learn-k8s';
+
+app.get('/listRequests', async (req, res) => {
+    const db = mongoClient.db(dbName);
+    const collection = db.collection('requests');
+
+    const requests = await collection.find({})
+
+    res.json(requests)
+})
+
+app.post('/addRequest', async (req, res) => {
+    const clientName = req.body.name
+
+    const db = mongoClient.db(dbName);
+    const collection = db.collection('requests');
+
+    const request = await collection.insertOne({
+        clientName,
+        time: new Date().toISOString()
+    })
+
+    res.json(request)
+})
+
+app.listen(port, async () => {
+    console.log(`Listening on port ${port}`);
+    await mongoClient.connect();
+})
---
+++ service/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.PORT || 3000,
+    mongoHost: process.env.MONGO_IP || 'localhost',
+}
